8955eba1bc0049c086a715315f6f2d195b690556
jmvieiro <jmvieiro@gmail.com>
2021-09-10 02:18:38 -0300

    avances2

--- src/containers/landing.jsx
+++ src/containers/landing.jsx
@@ -15,7 +15,7 @@ const Landing = () => {
       <div
         class="row"
         style={{
-          width: "100%",
+          width: "102%",
           position: "absolute",
           top: "40%",
           height: "60vh",
@@ -34,7 +34,7 @@ const Landing = () => {
       <div
         class="row"
         style={{
-          width: "100%",
+          width: "102%",
           position: "absolute",
           top: "40%",
           height: "60vh",
